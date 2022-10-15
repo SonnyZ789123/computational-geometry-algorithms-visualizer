de7af78d74555be3325d90f70f756d1712065b8e
SonnyZ789123 <yoran.mertens@telenet.be>
2022-10-15 17:41:06 +0200

    Add tests brute force convex hull

---
+++ src/lib/algorithms/convexHull.ts
@@ -0,0 +1,33 @@
+import { Vertex } from '../../types';
+
+/**
+ *
+ * @param {Vertex[]} vertices - A set P of points in the plane
+ *
+ * @returns {Vertex[]} A list containing the vertices of the convex hull in clockwise order
+ */
+export function bruteForceConvexHull(vertices: Set<Vertex>): Vertex[] {}
+
+/**
+ *
+ * @param {Vertex[]} vertices - A set P of points in the plane
+ *
+ * @returns {Vertex[]} A list containing the vertices of the convex hull in clockwise order
+ */
+// export function AndrewConvexHull(vertices: Set<Vertex>): Vertex[] {}
+
+/**
+ *
+ * @param {Vertex[]} vertices - A set P of points in the plane
+ *
+ * @returns {Vertex[]} A list containing the vertices of the convex hull in clockwise order
+ */
+// export function GrahamsScanConvexHull(vertices: Set<Vertex>): Vertex[] {}
+
+/**
+ *
+ * @param {Vertex[]} vertices - A set P of points in the plane
+ *
+ * @returns {Vertex[]} A list containing the vertices of the convex hull in clockwise order
+ */
+// export function JarvinsMarchConvexHull(vertices: Set<Vertex>): Vertex[] {}
---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+// _common
+export type Vertex = {
+  x: number;
+  y: number;
+};
+
+export type Edge = Set<Point>;
+
+// Convex Hull
+export type ConvexHull = Point[];
